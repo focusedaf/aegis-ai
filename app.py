a0ce9cf58acb6d332b820692eb156eede206aee4
focusedaf <andbeyondinfinity66@gmail.com>
2026-01-14 04:49:55 +0530

    img upload n receiving sorted

--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
+
+UPLOAD_DIR = "uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 @app.route("/")
 def index():
     return "wsup"
+
+# simulates img upload
+@app.route("/upload", methods=["POST"])
+def upload():
+    if "file" not in request.files:
+        return {"error": "no file"}, 400
+
+    file = request.files["file"]
+    filename = secure_filename(file.filename)
+    path = os.path.join(UPLOAD_DIR, filename)
+    file.save(path)
+
+    return jsonify({
+        "status": "uploaded",
+        "path": path
+    })
+
+# receive img
+@app.route("/receive",methods=["POST"])
+def receiveImg():
+    data = request.json
+    path = data.get("file_path")
+
+    if not path:
+        return {"error": "no file_path"}, 400
+
+    return {"status": "received", "path": path}
+    
+
 
 if __name__ in "__main__":
     app.run(debug=True)
